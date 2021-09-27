be0461640edcd10413a28861a2a2589d04fb9623
Ben Webb <ben.webb@candide.com>
2021-09-27 23:49:11 +0100

    Add some styling to home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,20 @@
-import { Flex, Text, Image } from '@chakra-ui/react'
+import { useEffect, useState, useRef } from 'react'
+import {
+  Flex,
+  Text,
+  Image,
+  Icon,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Button,
+} from '@chakra-ui/react'
+import format from 'date-fns/format'
 import { Link } from 'react-router-dom'
-
+import { ImWindows } from 'react-icons/im'
 interface GameButtonProps {
   title: string
   src: string
@@ -12,17 +26,133 @@ const GameButton = ({ title, src, link }: GameButtonProps) => {
     <Flex direction="column" justify="center" maxWidth="80px" cursor="pointer">
       <Link to={link}>
         <Image src={src} h={'80px'} w={'80px'} />
-        <Text align="center">{title}</Text>
+        <Text align="center" color="white" mt={2}>
+          {title}
+        </Text>
       </Link>
     </Flex>
   )
 }
 
-export default function Home() {
+interface ErrorWindowProps {
+  isOpen: boolean
+  onClose: () => void
+}
+
+function ErrorWindow({ isOpen, onClose }: ErrorWindowProps) {
+  const cancelRef = useRef(null)
+
   return (
-    <Flex direction="column">
-      <Text>Home Page</Text>
+    <AlertDialog
+      isOpen={isOpen}
+      onClose={onClose}
+      leastDestructiveRef={cancelRef}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent
+          borderRadius={0}
+          bgColor="#C0C0C0"
+          borderWidth="2px"
+          borderTopColor="lightGrey"
+          borderLeftColor="lightGrey"
+          borderRightColor="Gray"
+          borderBottomColor="Gray"
+        >
+          <AlertDialogHeader
+            fontSize="lg"
+            fontWeight="bold"
+            p={1}
+            m={0}
+            borderRadius={0}
+            bgColor={'blue'}
+            color="white"
+            borderWidth="2px"
+            borderTopColor="white"
+            borderLeftColor="white"
+            borderRightColor="white"
+            borderBottom="none"
+          >
+            Error error?
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            Maybe an error occured, we are unsure, try again?
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button onClick={onClose} ref={cancelRef} height="40px">
+              Cancel
+            </Button>
+            <Button onClick={onClose} ml={3}>
+              Ok
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
+
+export default function Home() {
+  const [date, setDate] = useState(new Date())
+  const [isOpen, setIsOpen] = useState(false)
+  const onErrorClose = () => setIsOpen(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDate(new Date())
+    }, 10000)
+  }, [date])
+
+  return (
+    <Flex direction="column" h="100vh" bgColor="darkerBlue" justify="center">
       <GameButton title="Solitaire" src="SolitaireIcon.png" link="/solitaire" />
+      <Flex
+        w="100%"
+        h="50px"
+        p={1}
+        bgColor="grey"
+        borderTopWidth="2px"
+        borderTopColor="lightGrey"
+        position="fixed"
+        bottom={0}
+        justify="space-between"
+      >
+        <Flex
+          w="110px"
+          h="100%"
+          p={1}
+          align="center"
+          justify="center"
+          bgColor="grey"
+          borderWidth="2px"
+          borderTopColor="lightGrey"
+          borderLeftColor="lightGrey"
+          borderRightColor="black"
+          borderBottomColor="black"
+          cursor="pointer"
+          onClick={() => setIsOpen(true)}
+        >
+          <Icon as={ImWindows} height="90%" width="40%" color="black" />
+          <Text color="black" letterSpacing="1.3px" fontWeight={600}>
+            Start
+          </Text>
+        </Flex>
+        <Flex
+          justify="center"
+          align="center"
+          borderWidth="1px"
+          borderTopColor="black"
+          borderLeftColor="black"
+          borderRightColor="lightGrey"
+          borderBottomColor="lightGrey"
+          px={3}
+        >
+          <Text color="black">{format(date, 'hh:mm a')}</Text>
+        </Flex>
+      </Flex>
+
+      <ErrorWindow isOpen={isOpen} onClose={onErrorClose} />
     </Flex>
   )
 }
