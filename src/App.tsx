9d6292287c04e9f7c0119473d047bdf0a5a51e1b
Ben Webb <b-webb@outlook.com>
2022-03-12 17:03:29 +0000

    Start NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import Home from "components/pages/Home"
 import {Route, Routes} from "react-router-dom"
 import MinesweeperBoard from "components/Minesweeper/GameBoard"
 import SolitiareBoard from "components/Solitaire/GameBoard"
+import {NavBar} from "./components/NavBar"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/solitiare" element={<SolitiareBoard />} />
-      <Route path="/minesweeper" element={<MinesweeperBoard />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/solitiare" element={<SolitiareBoard />} />
+        <Route path="/minesweeper" element={<MinesweeperBoard />} />
+      </Routes>
+    </>
   )
 }
 
