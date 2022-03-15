7cd4ef4375635fb9ff20b1911a63498b9974f750
Ben Webb <b-webb@outlook.com>
2022-03-15 20:31:37 +0000

    Add some shit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,58 @@
-import {Flex, Image} from "@chakra-ui/react"
+import {useState} from "react"
+import {
+  Flex,
+  Image,
+  Drawer,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerBody,
+  DrawerFooter,
+  Button,
+} from "@chakra-ui/react"
+import {GiHamburgerMenu} from "react-icons/gi"
 import {Link} from "react-router-dom"
 
 export const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
   return (
-    <Flex
-      w="100%"
-      h="70px"
-      bg="black"
-      dir="row"
-      p="4px"
-      color="white"
-      fontFamily="Noto Sans"
-    >
-      <Link to={"/"}>
-        <Image src="./Logo.png" height="100%" />
-      </Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/apps"}>Apps</Link>
-      <Link to={"/contact"}>Contact</Link>
-    </Flex>
+    <>
+      <Flex
+        w="100%"
+        h="70px"
+        bg="black"
+        dir="row"
+        p="4px"
+        color="white"
+        fontFamily="Noto Sans"
+      >
+        <Link to={"/"}>
+          <Image src="./Logo.png" height="100%" />
+        </Link>
+        <Button>
+          <GiHamburgerMenu />
+        </Button>
+      </Flex>
+      <Drawer isOpen={isOpen} placement="left" onClose={handleClose}>
+        <DrawerContent>
+          <DrawerCloseButton />
+
+          <DrawerBody>
+            <Link to={"/about"}>About</Link>
+            <Link to={"/apps"}>Apps</Link>
+            <Link to={"/contact"}>Contact</Link>{" "}
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={() => {}}>
+              Cancel
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }
--- src/components/Solitaire/furniture/GameCardTop.tsx
+++ src/components/Solitaire/furniture/GameCardTop.tsx
@@ -1,6 +1,6 @@
-import { CardProps, SelectedCardPositionEnum } from 'components/Solitaire/types'
-import { Flex } from '@chakra-ui/react'
-import { CardValueRow } from './CardValueRow'
+import {CardProps, SelectedCardPositionEnum} from "components/Solitaire/types"
+import {Flex} from "@chakra-ui/react"
+import {CardValueRow} from "./CardValueRow"
 
 interface GameCardTopProps extends CardProps {
   zindex: number
@@ -12,32 +12,32 @@ const getCardTopProps = (
 ) => {
   if (selectedCard && position === SelectedCardPositionEnum.top) {
     return {
-      h: '35px',
-      w: '100px',
-      bg: 'offOrange',
-      borderRadius: '8px 8px 0 0',
-      p: '5px 0 0 5px',
-      marginLeft: '-5px',
-      marginTop: '-5px',
+      h: "35px",
+      w: "100px",
+      bg: "offOrange",
+      borderRadius: "8px 8px 0 0",
+      p: "5px 0 0 5px",
+      marginLeft: "-5px",
+      marginTop: "-5px",
     }
   }
 
   if (position === SelectedCardPositionEnum.middle) {
     return {
-      h: '30px',
-      w: '100px',
-      bg: 'offOrange',
-      borderRadius: '0 0 0 0',
-      px: '5px',
-      marginLeft: '-5px',
-      marginTop: '0',
+      h: "30px",
+      w: "100px",
+      bg: "offOrange",
+      borderRadius: "0 0 0 0",
+      px: "5px",
+      marginLeft: "-5px",
+      marginTop: "0",
     }
   }
 
   return {
-    h: '30px',
-    w: '90px',
-    borderRadius: '0 0 8px 8px',
+    h: "30px",
+    w: "90px",
+    borderRadius: "0 0 8px 8px",
     p: 0,
     marginLeft: 0,
     marginTop: 0,
@@ -58,20 +58,20 @@ export const GameCardTop = ({
   const cardTopProps = getCardTopProps(cardIsSelected, position)
   return (
     <Flex {...cardTopProps}>
-      <Flex bgColor={isFirst || cardIsSelected ? 'none' : 'white'}>
+      <Flex bgColor={isFirst || cardIsSelected ? "none" : "white"}>
         <Flex
-          zindex={zindex}
+          zIndex={zindex}
           h="30px"
           w="90px"
           bg="white"
           boxShadow={
             position === null
-              ? '0 0 2px 2px rgba(0,0,0,0.2)'
-              : '0 -1px 1px 1px rgba(0,0,0,0.1)'
+              ? "0 0 2px 2px rgba(0,0,0,0.2)"
+              : "0 -1px 1px 1px rgba(0,0,0,0.1)"
           }
           borderRadius="8px 8px 0 0"
           p="5px 5px 0 5px"
-          onClick={e => {
+          onClick={(e) => {
             e.stopPropagation()
             onClick()
           }}
@@ -79,7 +79,7 @@ export const GameCardTop = ({
           <Flex
             h="100%"
             w="100%"
-            bg={faceUp === true ? 'white' : 'primaryBlue'}
+            bg={faceUp === true ? "white" : "primaryBlue"}
             borderRadius="4px 4px 0 0"
             direction="column"
             justify="space-between"
