14031827e25280c4620c3a20454e9ce7e9147827
Ben Webb <b-webb@outlook.com>
2022-03-15 23:48:48 +0000

    Add navbar to website

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,58 +1,44 @@
-import {useState} from "react"
-import {
-  Flex,
-  Image,
-  Drawer,
-  DrawerContent,
-  DrawerCloseButton,
-  DrawerBody,
-  DrawerFooter,
-  Button,
-} from "@chakra-ui/react"
-import {GiHamburgerMenu} from "react-icons/gi"
+import {Flex} from "@chakra-ui/react"
 import {Link} from "react-router-dom"
 
-export const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(true)
+import "./NavBar.css"
 
-  const handleClose = () => {
-    setIsOpen(false)
-  }
+const NavLink = ({title, link}: {title: string; link: string}) => (
+  <Link to={link}>
+    <Flex
+      className={"NavBarLink"}
+      border="solid 2px white"
+      borderRadius={3}
+      w={75}
+      py={1}
+      justify="center"
+    >
+      {title}
+    </Flex>
+  </Link>
+)
+
+export const NavBar = () => {
   return (
     <>
       <Flex
         w="100%"
-        h="70px"
-        bg="black"
+        bg="#222222"
+        h={70}
         dir="row"
         p="4px"
         color="white"
         fontFamily="Noto Sans"
+        justify="center"
+        alignItems="center"
       >
-        <Link to={"/"}>
-          <Image src="./Logo.png" height="100%" />
-        </Link>
-        <Button>
-          <GiHamburgerMenu />
-        </Button>
+        <Flex width={[320]} justify="space-between">
+          <NavLink link="/" title="Home" />
+          <NavLink link="/apps" title="Apps" />
+          <NavLink link="/about" title="About" />
+          <NavLink link="/contact" title="Contact" />
+        </Flex>
       </Flex>
-      <Drawer isOpen={isOpen} placement="left" onClose={handleClose}>
-        <DrawerContent>
-          <DrawerCloseButton />
-
-          <DrawerBody>
-            <Link to={"/about"}>About</Link>
-            <Link to={"/apps"}>Apps</Link>
-            <Link to={"/contact"}>Contact</Link>{" "}
-          </DrawerBody>
-
-          <DrawerFooter>
-            <Button variant="outline" mr={3} onClick={() => {}}>
-              Cancel
-            </Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
     </>
   )
 }
