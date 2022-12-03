4499d80b10740bee322fd54b4dcebd9a185099b2
Ben Webb <b-webb@outlook.com>
2022-12-03 00:03:47 +0000

    Look at some errors:

--- pages/auth/recipe/[recipeId].tsx
+++ pages/auth/recipe/[recipeId].tsx
@@ -1,13 +1,13 @@
 import AuthedPage from "../../../Components/Layout/AuthedPage"
 import Section from "../../../Components/Common/Section"
-import {currentUser} from "../../../FirebaseApi/auth"
-import {updateRecipeForUser} from "../../../FirebaseApi/database"
-import {updateRecipe} from "../../../Redux/slices/recipeSlice"
-import {useDispatch} from "react-redux"
-import {EditRecipe} from "../../../Components/Common/EditRecipe"
-import {useState} from "react"
-import {useTypedSelector} from "../../../hooks/typedSelector"
-import {useRouter} from "next/router"
+import { currentUser } from "../../../FirebaseApi/auth"
+import { updateRecipeForUser } from "../../../FirebaseApi/database"
+import { updateRecipe } from "../../../Redux/slices/recipeSlice"
+import { useDispatch } from "react-redux"
+import { EditRecipe } from "../../../Components/Common/EditRecipe"
+import { useEffect, useState } from "react"
+import { useTypedSelector } from "../../../hooks/typedSelector"
+import { useRouter } from "next/router"
 
 export default function HiddenRecipesEdit() {
   const [loading, setLoading] = useState(false)
@@ -16,51 +16,56 @@ export default function HiddenRecipesEdit() {
   const dispatch = useDispatch()
   const recipes = useTypedSelector((state) => state.recipes.recipes)
 
-  const {recipeId} = router.query
-
-  if (!recipeId) {
-    return router.push("/auth")
-  }
+  const { recipeId } = router.query
+  useEffect(() => {
+    if (!recipeId) {
+      router.push("/auth")
+    }
+  }, [recipeId, router])
 
   const recipe = recipes.find((rec) => rec.id === recipeId)
 
-  if (!recipe) {
-    return router.push("/auth")
-  }
+  useEffect(() => {
+    if (!recipe) {
+      router.push("/auth")
+    }
+  }, [recipe, router])
 
   return (
     <AuthedPage>
       <Section pt={[0, 0, 0]}>
-        <EditRecipe
-          title="Edit Recipe"
-          loading={loading}
-          recipe={recipe}
-          onSubmit={async (editedRecipe) => {
-            if (!user) {
-              return
-            }
+        {recipe && (
+          <EditRecipe
+            title="Edit Recipe"
+            loading={loading}
+            recipe={recipe}
+            onSubmit={async (editedRecipe) => {
+              if (!user) {
+                return
+              }
 
-            if (!editedRecipe) {
-              return
-            }
-            setLoading(true)
+              if (!editedRecipe) {
+                return
+              }
+              setLoading(true)
 
-            const recipeToSubmit = {...editedRecipe, id: recipe.id}
+              const recipeToSubmit = { ...editedRecipe, id: recipe.id }
 
-            const newRecipe = await updateRecipeForUser(
-              user.uid,
-              recipeToSubmit,
-            )
-            if (newRecipe.id === null) {
+              const newRecipe = await updateRecipeForUser(
+                user.uid,
+                recipeToSubmit
+              )
+              if (newRecipe.id === null) {
+                setLoading(false)
+                return
+              }
+
+              dispatch(updateRecipe(recipeToSubmit))
               setLoading(false)
-              return
-            }
-
-            dispatch(updateRecipe(recipeToSubmit))
-            setLoading(false)
-            router.push("/auth")
-          }}
-        />
+              router.push("/auth")
+            }}
+          />
+        )}
       </Section>
     </AuthedPage>
   )
